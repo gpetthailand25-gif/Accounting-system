762e2a11a374695a1a4506d41c862d5d684e3dc9
gpetthailand25-gif <gpet.thailand25@gmail.com>
2026-08-14 12:28:18 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase config สำหรับโปรเจกต์ accounting-system-3f351
+const firebaseConfig = {
+  apiKey: "AIzaSyAmbQofn-qcshN0iQqh9i3Sy1ejwsbm4wA",
+  authDomain: "accounting-system-3f351.firebaseapp.com",
+  projectId: "accounting-system-3f351",
+  storageBucket: "accounting-system-3f351.firebasestorage.app",
+  messagingSenderId: "228014108084",
+  appId: "1:228014108084:web:08d0204b94428cb68996e1",
+  measurementId: "G-7L276ZRFLK"
+};
+
+firebase.initializeApp(firebaseConfig);
